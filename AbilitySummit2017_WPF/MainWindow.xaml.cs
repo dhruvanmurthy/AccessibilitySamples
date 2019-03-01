d11435cb453645b8a0244482b3df4167abadb862
Dhruva Narasimha Murthy <dhruvanmurthy@users.noreply.github.com>
2019-03-01 14:50:50 -0800

    Added Live Region Testing code

--- AbilitySummit2017_WPF/MainWindow.xaml.cs
+++ AbilitySummit2017_WPF/MainWindow.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Automation;
+using System.Windows.Automation.Peers;
 
 namespace AbilitySummit2017_WPF
 {
@@ -12,8 +9,8 @@ namespace AbilitySummit2017_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        // Note: WPF apps can create custom AutomationPeers to enhance the 
-        // accessibility of their UI. 
+        // Note: WPF apps can create custom AutomationPeers to enhance the
+        // accessibility of their UI.
 
         // Note: All text exposed programmatically must be as localized as
         // text displayed visually.
@@ -34,73 +31,16 @@ namespace AbilitySummit2017_WPF
 
             // ACCESSIBILITY BUG FIX:
             // Note: WPF does not support LiveRegions and so Narrator will not announce
-            // the above change made to the static label. One option here would be to 
-            // pop up a message conveying the current status. Instead, for this demo, 
+            // the above change made to the static label. One option here would be to
+            // pop up a message conveying the current status. Instead, for this demo,
             // set the UIA ItemStatus property of the button to be the current status.
 
-            //this line of code Works Correctly in the this App.(modified and checked)
-            //like the below line We have added the same to our WaterMark.xaml ,but change not replicated 
-            // Shelveset User Name : V-SUNKAN
-            // Shelveset  Name : Loading_Narrator_KarthickNadarajan. 
-
-            AutomationProperties.SetItemStatus(ProcessButton, StatusButton.Text); 
-
-            //Below snippet not working. given by alan ren.
-            try
-            {
-                if (typeof(Label).GetProperties().Any((property) =>
-                {
-                    return property.Name == "LiveSetting";
-                }))
-                {
-                    Type liveSettingEnum = typeof(Label).Assembly.GetTypes().First((type) => { return type.Name == "AutomationLiveSetting"; });
-                    var values = Enum.GetValues(liveSettingEnum);
-                    object enumValue = null;
-                    foreach (object val in values)
-                    {
-                        if (val.ToString() == "Polite")
-                        {
-                            enumValue = val;
-                            break;
-                        }
-                    }
-                    typeof(Label).InvokeMember("LiveSetting", BindingFlags.Instance | BindingFlags.Public | BindingFlags.SetProperty, Type.DefaultBinder, "Narrator Reading Snippet Label", new object[] { enumValue });
-                }
-            }
-            catch(Exception ex)
-            {
-
-            }
-           
+            AutomationProperties.SetItemStatus(ProcessButton, StatusButton.Text);
         }
 
-        private void doubleclick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void LiveRegionButton_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                if (typeof(Label).GetProperties().Any((property) =>
-                {
-                    return property.Name == "LiveSetting";
-                }))
-                {
-                    Type liveSettingEnum = typeof(Label).Assembly.GetTypes().First((type) => { return type.Name == "AutomationLiveSetting"; });
-                    var values = Enum.GetValues(liveSettingEnum);
-                    object enumValue = null;
-                    foreach (object val in values)
-                    {
-                        if (val.ToString() == "Polite")
-                        {
-                            enumValue = val;
-                            break;
-                        }
-                    }
-                    typeof(Label).InvokeMember("LiveSetting", BindingFlags.Instance | BindingFlags.Public | BindingFlags.SetProperty, Type.DefaultBinder, MyLabel, new object[] { enumValue });
-                }
-            }
-            catch (Exception ex)
-            {
-
-            }
+            UIElementAutomationPeer.FromElement(LiveRegionText)?.RaiseAutomationEvent(AutomationEvents.LiveRegionChanged);
         }
     }
 }
